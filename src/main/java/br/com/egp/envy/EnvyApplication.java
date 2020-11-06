f96a5d1481fe2b5e75b4cbc94edeb31fcd4228e0
emerson.junior <emerson.junior@sankhya.com.br>
2020-11-06 11:45:11 -0300

    config heroku

--- src/main/java/br/com/egp/envy/EnvyApplication.java
+++ src/main/java/br/com/egp/envy/EnvyApplication.java
@@ -2,9 +2,10 @@ package br.com.egp.envy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EnvyApplication {
+public class EnvyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnvyApplication.class, args);
