8f893015fabf02159d73075aa933d804d0797888
Emerson Ribeiro <emersonrjr03@gmail.com>
2020-12-14 15:55:31 -0300

    adding cors handling

--- src/main/java/br/com/egp/envy/configuration/SecurityConfig.java
+++ src/main/java/br/com/egp/envy/configuration/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         if(Arrays.asList(environment.getActiveProfiles()).contains("test")) {
             http.headers().frameOptions().disable();
         }
-        http.csrf().and().cors().disable();
+        http.cors().and().csrf().disable();
         http.authorizeRequests()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
